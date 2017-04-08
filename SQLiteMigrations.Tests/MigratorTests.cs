c055e17ad97494513392a99796929c6833ed2f34
JC Casas <jace.casas@gmail.com>
2017-04-08 04:24:10 -0500

    add test to ensure timestamp format migration ids work

--- SQLiteMigrations.Tests/MigratorTests.cs
+++ SQLiteMigrations.Tests/MigratorTests.cs
@@ -19,10 +19,10 @@ namespace SQLiteMigrations.Tests
     [TestFixture]
     public class MigratorTests
     {
-		string temp_path;
+        string temp_path;
 
         [SetUp]
-		protected void SetUp()
+        protected void SetUp()
         {
             SQLitePCL.Batteries_V2.Init();
 
@@ -37,7 +37,7 @@ namespace SQLiteMigrations.Tests
         }
 
         [Test]
-		public void DatabaseFileCreated()
+        public void DatabaseFileCreated()
         {
             var workDirPath = Path.Combine(temp_path, "database_file_created");
             Directory.CreateDirectory(workDirPath);
@@ -55,98 +55,98 @@ namespace SQLiteMigrations.Tests
             Assert.True(File.Exists(DBPath), string.Format("Database file should exist"));
         }
 
-		[Test]
-		public void MigrationsTableCreated()
-		{
-			var workDirPath = Path.Combine(temp_path, "migrations_table_created");
-			Directory.CreateDirectory(workDirPath);
+        [Test]
+        public void MigrationsTableCreated()
+        {
+            var workDirPath = Path.Combine(temp_path, "migrations_table_created");
+            Directory.CreateDirectory(workDirPath);
 
-			var DBPath = Path.Combine(workDirPath, "db.sqlite3");
+            var DBPath = Path.Combine(workDirPath, "db.sqlite3");
 
             var Migrations = new Dictionary<string, string>();
 
-			var config = new Configuration
-			{
-				DBPath = DBPath,
+            var config = new Configuration
+            {
+                DBPath = DBPath,
                 MigrationsProvider = new TestMigrationsProvider
                 {
                     MigrationsDictionary = Migrations
                 }
-			};
+            };
 
-			var migrator = new SQLiteMigrator(config);
-			migrator.init();
+            var migrator = new SQLiteMigrator(config);
+            migrator.init();
 
             using (var db = SQLite3.Open(DBPath))
             {
-				var exists = db.Query("SELECT COUNT(*) FROM sqlite_master WHERE name = ? AND type = ?", "__migrations", "table").SelectScalarInt().First() > 0;
+                var exists = db.Query("SELECT COUNT(*) FROM sqlite_master WHERE name = ? AND type = ?", "__migrations", "table").SelectScalarInt64().First() > 0;
                 Assert.True(exists, "Table __migrations should exist");
             }
-		}
+        }
 
-		[Test]
-		public void MigrationIdSetted()
-		{
-			var workDirPath = Path.Combine(temp_path, "migration_id_setted");
-			Directory.CreateDirectory(workDirPath);
+        [Test]
+        public void MigrationIdSetted()
+        {
+            var workDirPath = Path.Combine(temp_path, "migration_id_setted");
+            Directory.CreateDirectory(workDirPath);
 
-			var DBPath = Path.Combine(workDirPath, "db.sqlite3");
+            var DBPath = Path.Combine(workDirPath, "db.sqlite3");
 
-			var Migrations = new Dictionary<string, string>();
-			Migrations["123_inicial.sql"] = string.Join(
-				"\n",
-				new string[]
-				{
-					"--- database id 123",
-					"--- database up",
-					"SELECT 'a'",
-					"--- database down",
-					"SELECT 'a'"
-				}
-			);
+            var Migrations = new Dictionary<string, string>();
+            Migrations["123_inicial.sql"] = string.Join(
+                "\n",
+                new string[]
+                {
+                    "--- database id 123",
+                    "--- database up",
+                    "SELECT 'a'",
+                    "--- database down",
+                    "SELECT 'a'"
+                }
+            );
 
-			var config = new Configuration
-			{
-				DBPath = DBPath,
-				MigrationsProvider = new TestMigrationsProvider
-				{
-					MigrationsDictionary = Migrations
-				}
-			};
+            var config = new Configuration
+            {
+                DBPath = DBPath,
+                MigrationsProvider = new TestMigrationsProvider
+                {
+                    MigrationsDictionary = Migrations
+                }
+            };
 
-			var migrator = new SQLiteMigrator(config);
-			migrator.init();
+            var migrator = new SQLiteMigrator(config);
+            migrator.init();
 
-			using (var db = SQLite3.Open(DBPath))
-			{
-				var current = db.Query("SELECT id FROM __migrations ORDER BY id DESC LIMIT 1").SelectScalarInt().First();
-				Assert.AreEqual(current, 123, "Current database version should be 123");
-			}
-		}
+            using (var db = SQLite3.Open(DBPath))
+            {
+                var current = db.Query("SELECT id FROM __migrations ORDER BY id DESC LIMIT 1").SelectScalarInt64().First();
+                Assert.AreEqual(current, 123, "Current database version should be 123");
+            }
+        }
 
-		[Test]
-		public void MultipleMigrationsCanCreateAndPopulateDatabase()
-		{
-			var workDirPath = Path.Combine(temp_path, "multiple_migrations_can_create_and_populate_database");
-			Directory.CreateDirectory(workDirPath);
+        [Test]
+        public void MultipleMigrationsCanCreateAndPopulateDatabase()
+        {
+            var workDirPath = Path.Combine(temp_path, "multiple_migrations_can_create_and_populate_database");
+            Directory.CreateDirectory(workDirPath);
 
-			var DBPath = Path.Combine(workDirPath, "db.sqlite3");
+            var DBPath = Path.Combine(workDirPath, "db.sqlite3");
 
-			var Migrations = new Dictionary<string, string>();
-			Migrations["1_create.sql"] = string.Join(
-				"\n",
-				new string[]
-				{
-					"--- database id 1",
-					"--- database up",
-					"CREATE TABLE test (",
+            var Migrations = new Dictionary<string, string>();
+            Migrations["1_create.sql"] = string.Join(
+                "\n",
+                new string[]
+                {
+                    "--- database id 1",
+                    "--- database up",
+                    "CREATE TABLE test (",
                     "    id INT NOT NULL,",
                     "    name VARCHAR(10) NOT NULL",
                     ");",
-					"--- database down",
+                    "--- database down",
                     "DROP TABLE test;"
-				}
-			);
+                }
+            );
             Migrations["2_populate.sql"] = string.Join(
                 "\n",
                 new string[]
@@ -155,43 +155,83 @@ namespace SQLiteMigrations.Tests
                     "--- database up",
                     "INSERT INTO test",
                     "    (id, name)",
-    				"VALUES",
+                    "VALUES",
                     "    (1, 'Jacinto'),",
                     "    (2, 'Susana');",
-					"--- database down",
+                    "--- database down",
                     "DELETE FROM test WHERE id IN (1, 2);"
-				}
-			);
+                }
+            );
 
-			var config = new Configuration
-			{
-				DBPath = DBPath,
-				MigrationsProvider = new TestMigrationsProvider
-				{
-					MigrationsDictionary = Migrations
-				}
-			};
+            var config = new Configuration
+            {
+                DBPath = DBPath,
+                MigrationsProvider = new TestMigrationsProvider
+                {
+                    MigrationsDictionary = Migrations
+                }
+            };
 
-			var migrator = new SQLiteMigrator(config);
-			migrator.init();
+            var migrator = new SQLiteMigrator(config);
+            migrator.init();
 
-			using (var db = SQLite3.Open(DBPath))
-			{
+            using (var db = SQLite3.Open(DBPath))
+            {
                 var data = db.Query("SELECT id, name FROM test ORDER BY id ASC")
                              .Select((arg) => new Tuple<int, string>(arg[0].ToInt(), arg[1].ToString()))
                              .ToArray();
-                
+
                 Assert.AreEqual(data.Length, 2, "There should be 2 rows in the table");
 
                 var jacinto = data[0];
-				var susana = data[1];
+                var susana = data[1];
 
                 Assert.AreEqual(jacinto.Item1, 1, "Jacinto's ID should be 1");
                 Assert.AreEqual(jacinto.Item2, "Jacinto", "Jacinto's name should be Jacinto");
 
                 Assert.AreEqual(susana.Item1, 2, "Susana's ID should be 2");
                 Assert.AreEqual(susana.Item2, "Susana", "Susana's name should be Susana");
-			}
-		}
+            }
+        }
+
+        [Test]
+        public void TimestampIdFormatWorks()
+        {
+            var workDirPath = Path.Combine(temp_path, "timestamp_id_format_works");
+            Directory.CreateDirectory(workDirPath);
+
+            var DBPath = Path.Combine(workDirPath, "db.sqlite3");
+
+            var Migrations = new Dictionary<string, string>();
+            Migrations["201704080412_inicial.sql"] = string.Join(
+                "\n",
+                new string[]
+                {
+                    "--- database id 201704080412",
+                    "--- database up",
+                    "SELECT 'a'",
+                    "--- database down",
+                    "SELECT 'a'"
+                }
+            );
+
+            var config = new Configuration
+            {
+                DBPath = DBPath,
+                MigrationsProvider = new TestMigrationsProvider
+                {
+                    MigrationsDictionary = Migrations
+                }
+            };
+
+            var migrator = new SQLiteMigrator(config);
+            migrator.init();
+
+            using (var db = SQLite3.Open(DBPath))
+            {
+                var current = db.Query("SELECT id FROM __migrations ORDER BY id DESC LIMIT 1").SelectScalarInt64().First();
+                Assert.AreEqual(current, 201704080412, "Current database version should be 201704080412");
+            }
+        }
     }
 }
